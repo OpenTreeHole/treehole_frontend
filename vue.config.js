1d7fe80e0ce6a54b41710814f52924b5a0f0b5a6
Kylin24Kwok <1296845690@qq.com>
2021-04-17 00:27:04 +0800

    PWA Capability

--- vue.config.js
+++ vue.config.js
@@ -37,14 +37,14 @@ module.exports = {
       //   },
       // ]
     },
-    iconPaths: {
-      faviconSVG: '/img/icons/favicon.svg',
-      favicon32: '/img/icons/favicon-32x32.png',
-      favicon16: '/img/icons/favicon-16x16.png',
-      appleTouchIcon: '/img/icons/apple-touch-icon-152x152.png',
-      maskIcon: '/img/icons/safari-pinned-tab.svg',
-      msTileImage: '/img/icons/msapplication-icon-144x144.png'
-    }
+    // iconPaths: {
+    //   faviconSVG: '/img/icons/favicon.svg',
+    //   favicon32: '/img/icons/favicon-32x32.png',
+    //   favicon16: '/img/icons/favicon-16x16.png',
+    //   appleTouchIcon: '/img/icons/apple-touch-icon-152x152.png',
+    //   maskIcon: '/img/icons/safari-pinned-tab.svg',
+    //   msTileImage: '/img/icons/msapplication-icon-144x144.png'
+    // }
   },
   lintOnSave: false,
   chainWebpack: config => {
