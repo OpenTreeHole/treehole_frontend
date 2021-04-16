4f468e0f53b1be314dddd12e372d8560f7f64c8d
Kylin24Kwok <1296845690@qq.com>
2021-04-16 19:00:25 +0800

    icon capability

--- vue.config.js
+++ vue.config.js
@@ -7,16 +7,16 @@ module.exports = {
     msTileColor: "#4278ce",
     appleMobileWebAppCache: "yes",
     workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            swSrc: 'src/service-worker.js',
-            // ...other Workbox options...
-            exclude: [
-              /\.map$/, 
-              /manifest\.json$/,
-              /online\.json/,
-          ],
-        },
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js',
+      // ...other Workbox options...
+      exclude: [
+        /\.map$/,
+        /manifest\.json$/,
+        /online\.json/,
+      ],
+    },
     manifestOptions: {
       name: "FDU HOLE",
       short_name: "树洞",
@@ -26,36 +26,36 @@ module.exports = {
       theme_color: "#4278ce",
       background_color: '#ffffff',
       icons: [
-        {
-          "src": "/img/icons/android-chrome-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png"
-        },
-        {
-          "src": "/img/icons/android-chrome-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png"
-        },
-        {
-          "src": "/img/icons/android-chrome-maskable-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png"
-        },
-        {
-          "src": "/img/icons/android-chrome-maskable-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png"
-        },
-        {
-          "src": "/img/icons/apple-touch-icon-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png"
-        },
-        {
-          "src": "/img/icons/apple-touch-icon-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png"
-        },
+        // {
+        //   "src": "/img/icons/android-chrome-192x192.png",
+        //   "sizes": "192x192",
+        //   "type": "image/png"
+        // },
+        // {
+        //   "src": "/img/icons/android-chrome-512x512.png",
+        //   "sizes": "512x512",
+        //   "type": "image/png"
+        // },
+        // {
+        //   "src": "/img/icons/android-chrome-maskable-192x192.png",
+        //   "sizes": "192x192",
+        //   "type": "image/png"
+        // },
+        // {
+        //   "src": "/img/icons/android-chrome-maskable-512x512.png",
+        //   "sizes": "512x512",
+        //   "type": "image/png"
+        // },
+        // {
+        //   "src": "/img/icons/apple-touch-icon-192x192.png",
+        //   "sizes": "192x192",
+        //   "type": "image/png"
+        // },
+        // {
+        //   "src": "/img/icons/apple-touch-icon-512x512.png",
+        //   "sizes": "512x512",
+        //   "type": "image/png"
+        // },
         {
           "src": "/img/icons/icon-192x192.png",
           "sizes": "192x192",
@@ -74,7 +74,7 @@ module.exports = {
     config.plugin('provide').use(webpack.ProvidePlugin, [{
       'window.Quill': 'quill/dist/quill.js',
       Quill: 'quill/dist/quill.js'
-	    }])
+    }])
   },
   transpileDependencies: [
     'vuetify'
