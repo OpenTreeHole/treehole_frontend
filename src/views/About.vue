b2db674a88ab9cc77cbaaa723669e01f1984aa8e
Kylin24Kwok <1296845690@qq.com>
2021-04-17 01:36:56 +0800

    update About

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,22 +6,22 @@
         Made with ❤ by
         <a href="https://github.com/fduhole" target="_blank">FDU-Hole-Dev</a>
       </blockquote>
+      <v-btn color="primary" @click.stop="refreshAll">刷新应用</v-btn>
+      <p>如果遇到问题，或需要升级，请点击此按钮</p>
       <div class="installed" v-if="installed">
         <p>
           当前版本：{{ $feConfig.feVersion }}<br />
           最新版本：{{ latestVersion }}
         </p>
-        <p v-if="updateAvailable">
-          检测到新版本：
-          <v-btn color="primary" @click.stop="refreshAll">立即更新</v-btn>
-        </p>
+        <p v-if="updateAvailable">检测到新版本，刷新应用以升级！</p>
       </div>
       <div class="not-installed" v-if="!installed">
         请<span>安装本应用</span>以获得更好的体验！
       </div>
     </div>
-    <div id=""></div>
-    <div id="privacy-policy"></div>
+    <div id="community-agreement"><h2>社区公约</h2></div>
+    <div id="terms-of-use"><h2>使用条款</h2></div>
+    <div id="privacy-policy"><h2>隐私政策</h2></div>
   </v-container>
 </template>
 
