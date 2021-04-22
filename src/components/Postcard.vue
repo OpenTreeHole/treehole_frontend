443e41c15dded9a9b4667e2ea10dda13f94f3e0e
Kylin24Kwok <1296845690@qq.com>
2021-04-22 21:04:20 +0800

    自动折叠

--- src/components/Postcard.vue
+++ src/components/Postcard.vue
@@ -15,68 +15,78 @@
         {{ tag.name }}
       </v-chip>
     </v-card-text>
-
-    <!-- 内容主体 -->
     <v-card-text
-      @click="toDiscussion(discussion.id, index)"
-      class="text--primary py-2 text-body-1 clickable"
-      v-ripple
-    >
-      <div
-        v-if="this.$parent.styleData[index]['fold']"
-        :id="'p' + index"
-        class="fold"
+      class="folded-hint"
+      v-if="discussion.is_folded"
+      color="grey" 
+    >该内容已折叠：<span
+    @click="displayIt=!displayIt"
+    >{{
+      displayIt ? '收起' : '展开'
+    }}</span></v-card-text>
+    <div class="post-content" v-show="displayIt">
+      <!-- 内容主体 -->
+      <v-card-text
+        @click="toDiscussion(discussion.id, index)"
+        class="text--primary py-2 text-body-1 clickable"
+        v-ripple
       >
-        {{ discussion.first_post.content | plainText }}
-      </div>
-      <div v-else :id="'p' + index" class="unfold">
-        <div id="rich-text" v-html="discussion.first_post.content"></div>
-      </div>
-    </v-card-text>
-
-    <!-- 展开折叠按钮 -->
-    <div v-if="this.$parent.styleData[index]['lines'] > 3">
-      <div v-if="this.$parent.styleData[index]['fold']">
-        <v-btn
-          text
-          block
-          depressed
-          x-small
-          color="grey lighten-1"
-          @click="unfold(index)"
+        <div
+          v-if="this.$parent.styleData[index]['fold']"
+          :id="'p' + index"
+          class="fold"
         >
-          <v-icon>mdi-chevron-double-down</v-icon>
-        </v-btn>
-      </div>
+          {{ discussion.first_post.content | plainText }}
+        </div>
+        <div v-else :id="'p' + index" class="unfold">
+          <div id="rich-text" v-html="discussion.first_post.content"></div>
+        </div>
+      </v-card-text>
 
+      <!-- 展开折叠按钮 -->
+      <div v-if="this.$parent.styleData[index]['lines'] > 3">
+        <div v-if="this.$parent.styleData[index]['fold']">
+          <v-btn
+            text
+            block
+            depressed
+            x-small
+            color="grey lighten-1"
+            @click="unfold(index)"
+          >
+            <v-icon>mdi-chevron-double-down</v-icon>
+          </v-btn>
+        </div>
+
+        <div v-else>
+          <v-btn
+            text
+            block
+            depressed
+            x-small
+            color="grey lighten-1"
+            @click="fold(index)"
+          >
+            <v-icon>mdi-chevron-double-up</v-icon>
+          </v-btn>
+        </div>
+      </div>
       <div v-else>
-        <v-btn
-          text
-          block
-          depressed
-          x-small
-          color="grey lighten-1"
-          @click="fold(index)"
-        >
-          <v-icon>mdi-chevron-double-up</v-icon>
-        </v-btn>
+        <div style="height: 0.5rem"></div>
       </div>
-    </div>
-    <div v-else>
-      <div style="height: 0.5rem"></div>
-    </div>
 
-    <!-- 脚标 -->
-    <v-card-text class="pt-0 pb-0 text-center caption">
-      <span style="float: left">#{{ discussion['id'] }}</span>
-      <span style="float: inherit">{{
-        discussion['date_updated'] | timeDifference
-      }}</span>
-      <span style="float: right">
-        {{ discussion['count'] }}
-        <v-icon small>mdi-message-processing-outline</v-icon>
-      </span>
-    </v-card-text>
+      <!-- 脚标 -->
+      <v-card-text class="pt-0 pb-0 text-center caption">
+        <span style="float: left">#{{ discussion['id'] }}</span>
+        <span style="float: inherit">{{
+          discussion['date_updated'] | timeDifference
+        }}</span>
+        <span style="float: right">
+          {{ discussion['count'] }}
+          <v-icon small>mdi-message-processing-outline</v-icon>
+        </span>
+      </v-card-text>
+    </div>
   </v-card>
 </template>
 
@@ -86,6 +96,12 @@ export default {
   props: {
     discussion: {},
     index: '',
+  },
+  data() {
+    return {
+      displayIt: !this.discussion.is_folded,
+      // displayIt: true,
+    }
   },
   methods: {
     addTag(tag) {
