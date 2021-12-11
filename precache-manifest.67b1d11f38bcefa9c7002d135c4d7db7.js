577551e6b0ce8f4870e2f3e1e530755b8c3d0ef8
runner <runner@Mac-1639201036610.local>
2021-12-11 06:33:34 +0000

    gh-action deploy

---
+++ precache-manifest.67b1d11f38bcefa9c7002d135c4d7db7.js
@@ -0,0 +1,482 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934fb61bc787028cd2fee8f3d35ff814",
+    "url": "/vue/.well-known/apple-app-site-association"
+  },
+  {
+    "revision": "f71dce9eef5ae43ef647a8f87c0c734b",
+    "url": "/vue/33faf0fc8484607ccba3.worker.js"
+  },
+  {
+    "revision": "9d179a24b7a0cb0e0f28",
+    "url": "/vue/css/app.11416353.css"
+  },
+  {
+    "revision": "10e3a082f389e3d06d56",
+    "url": "/vue/css/chunk-vendors.6612c234.css"
+  },
+  {
+    "revision": "3e94a45ca214f93c9774291237bb393e",
+    "url": "/vue/fonts/KaTeX_AMS-Regular.3e94a45c.ttf"
+  },
+  {
+    "revision": "7bc59ee9f9ad43040a2ff34a912bec13",
+    "url": "/vue/fonts/KaTeX_AMS-Regular.7bc59ee9.woff"
+  },
+  {
+    "revision": "92297720a8aaa0d239940bf10509fa1f",
+    "url": "/vue/fonts/KaTeX_AMS-Regular.92297720.woff2"
+  },
+  {
+    "revision": "194a8b74eefdc37015fdbf7a2ad36e9f",
+    "url": "/vue/fonts/KaTeX_Caligraphic-Bold.194a8b74.woff"
+  },
+  {
+    "revision": "4c37056017b7f742eeb8851510f4971a",
+    "url": "/vue/fonts/KaTeX_Caligraphic-Bold.4c370560.ttf"
+  },
+  {
+    "revision": "7d1eb996b957fdf12cad6e323da6db5c",
+    "url": "/vue/fonts/KaTeX_Caligraphic-Bold.7d1eb996.woff2"
+  },
+  {
+    "revision": "096ac2cc25065eeca89ca560d0a51b1a",
+    "url": "/vue/fonts/KaTeX_Caligraphic-Regular.096ac2cc.ttf"
+  },
+  {
+    "revision": "4a09d055bfc3d5c43bd6cfcad061bca9",
+    "url": "/vue/fonts/KaTeX_Caligraphic-Regular.4a09d055.woff"
+  },
+  {
+    "revision": "b7703ab2622e72e54714135836b9da1a",
+    "url": "/vue/fonts/KaTeX_Caligraphic-Regular.b7703ab2.woff2"
+  },
+  {
+    "revision": "06e6c3f3900bf855fe69c6adbf2b59ac",
+    "url": "/vue/fonts/KaTeX_Fraktur-Bold.06e6c3f3.ttf"
+  },
+  {
+    "revision": "5ec2d70532268860be506b06c8c82615",
+    "url": "/vue/fonts/KaTeX_Fraktur-Bold.5ec2d705.woff2"
+  },
+  {
+    "revision": "8518f4fec3f73b37b3693f3625bc146b",
+    "url": "/vue/fonts/KaTeX_Fraktur-Bold.8518f4fe.woff"
+  },
+  {
+    "revision": "56d671ca5dc88ca20911bddc5a84c9fb",
+    "url": "/vue/fonts/KaTeX_Fraktur-Regular.56d671ca.woff2"
+  },
+  {
+    "revision": "c5953fefe37f48064e17b75ff607fc0d",
+    "url": "/vue/fonts/KaTeX_Fraktur-Regular.c5953fef.woff"
+  },
+  {
+    "revision": "e78f1f025d28668ddcaa2791fc818436",
+    "url": "/vue/fonts/KaTeX_Fraktur-Regular.e78f1f02.ttf"
+  },
+  {
+    "revision": "36af3e91a84645afe0e40be8046fba86",
+    "url": "/vue/fonts/KaTeX_Main-Bold.36af3e91.woff"
+  },
+  {
+    "revision": "5c6035670847d3c6f8af38393b04eee9",
+    "url": "/vue/fonts/KaTeX_Main-Bold.5c603567.ttf"
+  },
+  {
+    "revision": "d94e53ed77ae219e76e90cf02e7d627d",
+    "url": "/vue/fonts/KaTeX_Main-Bold.d94e53ed.woff2"
+  },
+  {
+    "revision": "addf865739892218570c00053c6c02dc",
+    "url": "/vue/fonts/KaTeX_Main-BoldItalic.addf8657.woff2"
+  },
+  {
+    "revision": "ceb7b93007c7e338f635833a4259a14b",
+    "url": "/vue/fonts/KaTeX_Main-BoldItalic.ceb7b930.woff"
+  },
+  {
+    "revision": "dde48012334937d446600e982f5d8522",
+    "url": "/vue/fonts/KaTeX_Main-BoldItalic.dde48012.ttf"
+  },
+  {
+    "revision": "2bc9206b7e2d0c75941d4930b1990e49",
+    "url": "/vue/fonts/KaTeX_Main-Italic.2bc9206b.woff"
+  },
+  {
+    "revision": "890a6b3ff7159ac41c71ccd2c4e76323",
+    "url": "/vue/fonts/KaTeX_Main-Italic.890a6b3f.ttf"
+  },
+  {
+    "revision": "c24bf2da2b956b13ba8101dda48a2d77",
+    "url": "/vue/fonts/KaTeX_Main-Italic.c24bf2da.woff2"
+  },
+  {
+    "revision": "16df5fa99e77eff4b91a4f3423b761d4",
+    "url": "/vue/fonts/KaTeX_Main-Regular.16df5fa9.woff"
+  },
+  {
+    "revision": "46b1ffa6b7af252c4f89e7c8d757a39f",
+    "url": "/vue/fonts/KaTeX_Main-Regular.46b1ffa6.woff2"
+  },
+  {
+    "revision": "d866ab9ba6d5654e524ec6bd5e1d5a14",
+    "url": "/vue/fonts/KaTeX_Main-Regular.d866ab9b.ttf"
+  },
+  {
+    "revision": "a562e8c1abe0de60ffc1f77ed9412327",
+    "url": "/vue/fonts/KaTeX_Math-BoldItalic.a562e8c1.woff"
+  },
+  {
+    "revision": "bb40192b2e03ed2f0fa8ee17ff587fd3",
+    "url": "/vue/fonts/KaTeX_Math-BoldItalic.bb40192b.woff2"
+  },
+  {
+    "revision": "cbf872382693c0b1c71f5df380206d7a",
+    "url": "/vue/fonts/KaTeX_Math-BoldItalic.cbf87238.ttf"
+  },
+  {
+    "revision": "92281c86717a2642f166728a8973f3f9",
+    "url": "/vue/fonts/KaTeX_Math-Italic.92281c86.woff"
+  },
+  {
+    "revision": "96c61839461dd0264a18a65a39212a62",
+    "url": "/vue/fonts/KaTeX_Math-Italic.96c61839.ttf"
+  },
+  {
+    "revision": "c359b8d5e269eaf907baa338587ca80e",
+    "url": "/vue/fonts/KaTeX_Math-Italic.c359b8d5.woff2"
+  },
+  {
+    "revision": "26f259e4e6c41f013a51f790e3a7dd0c",
+    "url": "/vue/fonts/KaTeX_SansSerif-Bold.26f259e4.woff"
+  },
+  {
+    "revision": "95392fb82420439d0c3bb9f2d414f83c",
+    "url": "/vue/fonts/KaTeX_SansSerif-Bold.95392fb8.woff2"
+  },
+  {
+    "revision": "cb7f745a9094fa659195d2d6f13711eb",
+    "url": "/vue/fonts/KaTeX_SansSerif-Bold.cb7f745a.ttf"
+  },
+  {
+    "revision": "7de8e4ae08eaaeb10a5112ce7f09ba80",
+    "url": "/vue/fonts/KaTeX_SansSerif-Italic.7de8e4ae.woff2"
+  },
+  {
+    "revision": "9c330d9e5674fd3e79c644e451240331",
+    "url": "/vue/fonts/KaTeX_SansSerif-Italic.9c330d9e.ttf"
+  },
+  {
+    "revision": "a8f101e81769f08428b1e01d4b04c033",
+    "url": "/vue/fonts/KaTeX_SansSerif-Italic.a8f101e8.woff"
+  },
+  {
+    "revision": "3ad86448e4a27c0e4a9b4e333aebeaa7",
+    "url": "/vue/fonts/KaTeX_SansSerif-Regular.3ad86448.woff2"
+  },
+  {
+    "revision": "63e176e62748655e2076a07a12cc9355",
+    "url": "/vue/fonts/KaTeX_SansSerif-Regular.63e176e6.ttf"
+  },
+  {
+    "revision": "df1117b1c61ba086c7c286b4132350d3",
+    "url": "/vue/fonts/KaTeX_SansSerif-Regular.df1117b1.woff"
+  },
+  {
+    "revision": "1e0cfc3296ecafedbd818c2dd8e42d44",
+    "url": "/vue/fonts/KaTeX_Script-Regular.1e0cfc32.woff"
+  },
+  {
+    "revision": "52316064070cd6860d779f6a1185633e",
+    "url": "/vue/fonts/KaTeX_Script-Regular.52316064.ttf"
+  },
+  {
+    "revision": "9003d8e8bd5d718f953b45d21235f424",
+    "url": "/vue/fonts/KaTeX_Script-Regular.9003d8e8.woff2"
+  },
+  {
+    "revision": "149a3821d5c9c13e75fffed96203aca7",
+    "url": "/vue/fonts/KaTeX_Size1-Regular.149a3821.woff"
+  },
+  {
+    "revision": "1eacb53819cf38ec1e4ffb5d777d4864",
+    "url": "/vue/fonts/KaTeX_Size1-Regular.1eacb538.woff2"
+  },
+  {
+    "revision": "f8369ce8f0adc37a0654c8e989499e7c",
+    "url": "/vue/fonts/KaTeX_Size1-Regular.f8369ce8.ttf"
+  },
+  {
+    "revision": "1fe5d5d629404094a867aa61068c9157",
+    "url": "/vue/fonts/KaTeX_Size2-Regular.1fe5d5d6.woff2"
+  },
+  {
+    "revision": "3e0929682a6641021102acd37fe253de",
+    "url": "/vue/fonts/KaTeX_Size2-Regular.3e092968.woff"
+  },
+  {
+    "revision": "90b50b453237cd955bbe7b979bb7345a",
+    "url": "/vue/fonts/KaTeX_Size2-Regular.90b50b45.ttf"
+  },
+  {
+    "revision": "0a94dd2f83ba3c455c02dcf740a0944a",
+    "url": "/vue/fonts/KaTeX_Size3-Regular.0a94dd2f.ttf"
+  },
+  {
+    "revision": "2e040dfd3e172645c8cd5dc3baac16d5",
+    "url": "/vue/fonts/KaTeX_Size3-Regular.2e040dfd.woff"
+  },
+  {
+    "revision": "70d01e490e97425d479807ea319ceed5",
+    "url": "/vue/fonts/KaTeX_Size4-Regular.70d01e49.ttf"
+  },
+  {
+    "revision": "a3fa8a08a95dab81913e04695b08cc32",
+    "url": "/vue/fonts/KaTeX_Size4-Regular.a3fa8a08.woff2"
+  },
+  {
+    "revision": "ce347ddfe670ca7dc3c61d6060441607",
+    "url": "/vue/fonts/KaTeX_Size4-Regular.ce347ddf.woff"
+  },
+  {
+    "revision": "099e00d5873cd2d4ef3ca5d23c23c313",
+    "url": "/vue/fonts/KaTeX_Typewriter-Regular.099e00d5.woff2"
+  },
+  {
+    "revision": "2f0057192d241cb99d64bba848b0164a",
+    "url": "/vue/fonts/KaTeX_Typewriter-Regular.2f005719.ttf"
+  },
+  {
+    "revision": "2f78c7d76fe78153d0ca675b3923507d",
+    "url": "/vue/fonts/KaTeX_Typewriter-Regular.2f78c7d7.woff"
+  },
+  {
+    "revision": "b92e93abd403291ac3b35a291b469737",
+    "url": "/vue/img/icons/android-chrome-144x144.png"
+  },
+  {
+    "revision": "9456469fafcb1bffb04d4e188a58a4c9",
+    "url": "/vue/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "fd9f7395571dbbadd9e4cab702dafaac",
+    "url": "/vue/img/icons/android-chrome-256x256.png"
+  },
+  {
+    "revision": "fba5efcab2bd41102144cd60665a7aac",
+    "url": "/vue/img/icons/android-chrome-36x36.png"
+  },
+  {
+    "revision": "1e0ebd07a9dae10b6be17ee4d565a083",
+    "url": "/vue/img/icons/android-chrome-384x384.png"
+  },
+  {
+    "revision": "6eba57edbf376025a638d7792ddd3ddf",
+    "url": "/vue/img/icons/android-chrome-48x48.png"
+  },
+  {
+    "revision": "c315d204a62f34a833d1049bc626f23f",
+    "url": "/vue/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "07596ffa537228993c1c5b26b15523b8",
+    "url": "/vue/img/icons/android-chrome-72x72.png"
+  },
+  {
+    "revision": "6e3aaf7e3e7b502bb38b0e5795a197a9",
+    "url": "/vue/img/icons/android-chrome-96x96.png"
+  },
+  {
+    "revision": "6cedebca68746bfd6da9789a5a600d1a",
+    "url": "/vue/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "ae31d7cf3099eeb867b24a829bd7741d",
+    "url": "/vue/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "788e46a457eb25fc4fb1fb00c456680e",
+    "url": "/vue/img/icons/apple-touch-icon-114x114-precomposed.png"
+  },
+  {
+    "revision": "cfefe4297d0e3e2b9c9337bb4ac1d2a1",
+    "url": "/vue/img/icons/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "df8ad96a0677ff138b3a55e359023b21",
+    "url": "/vue/img/icons/apple-touch-icon-120x120-precomposed.png"
+  },
+  {
+    "revision": "4f71fdfe2cc6dfa1ffe9d8c3cf7f6812",
+    "url": "/vue/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "d496943801a1c9ae3fd7f66ca2444a94",
+    "url": "/vue/img/icons/apple-touch-icon-144x144-precomposed.png"
+  },
+  {
+    "revision": "50e3e1a9d4f5634cb48556f38450ace4",
+    "url": "/vue/img/icons/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "9447eb7fa79614e34621702f063ce179",
+    "url": "/vue/img/icons/apple-touch-icon-152x152-precomposed.png"
+  },
+  {
+    "revision": "b202dca396dcfb0ad28273ffa3dd6776",
+    "url": "/vue/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "66bc38c4fdc5bdb6694262bd6e5093a3",
+    "url": "/vue/img/icons/apple-touch-icon-180x180-precomposed.png"
+  },
+  {
+    "revision": "479553780f39781a92d93430449904b6",
+    "url": "/vue/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "1e8b4bca081125a0baeef47a37e249dd",
+    "url": "/vue/img/icons/apple-touch-icon-192x192.png"
+  },
+  {
+    "revision": "f959ac3d36d271d5b563186812778bb4",
+    "url": "/vue/img/icons/apple-touch-icon-512x512.png"
+  },
+  {
+    "revision": "8cfc3636dee4ccd864b1b20b44b722e1",
+    "url": "/vue/img/icons/apple-touch-icon-57x57-precomposed.png"
+  },
+  {
+    "revision": "5596b90e0d35d240e9c84474676f9d0b",
+    "url": "/vue/img/icons/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "43d9bb973178a746151ffe082fdf864c",
+    "url": "/vue/img/icons/apple-touch-icon-60x60-precomposed.png"
+  },
+  {
+    "revision": "5f0f92e43071f0979e267f0e9796fadf",
+    "url": "/vue/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "2b89c6e4f7c23013d00b5a7bcd41e31a",
+    "url": "/vue/img/icons/apple-touch-icon-72x72-precomposed.png"
+  },
+  {
+    "revision": "a9c66a081b12e980e088dfad35dba8f8",
+    "url": "/vue/img/icons/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "27c3590fb68e22ba054bd113146bc948",
+    "url": "/vue/img/icons/apple-touch-icon-76x76-precomposed.png"
+  },
+  {
+    "revision": "89361921642a2c1a7c2aef10428e13bf",
+    "url": "/vue/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "ee3491a8e5a4a9b6d4db78b7fbe72839",
+    "url": "/vue/img/icons/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "833ca90f8ee00249e666e7ac1d31d188",
+    "url": "/vue/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4376013ba7fe78676993b1cea3b925dd",
+    "url": "/vue/img/icons/browserconfig.xml"
+  },
+  {
+    "revision": "cba402313b723947da2e937167154ab2",
+    "url": "/vue/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "671ead26de105d5114b6f2994b712e35",
+    "url": "/vue/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c5d0420d15c2add14ee8c246408d888a",
+    "url": "/vue/img/icons/favicon.ico"
+  },
+  {
+    "revision": "1f7ad5d670c290802d6e64562a3ec405",
+    "url": "/vue/img/icons/favicon.svg"
+  },
+  {
+    "revision": "1e8b4bca081125a0baeef47a37e249dd",
+    "url": "/vue/img/icons/icon-192x192.png"
+  },
+  {
+    "revision": "f959ac3d36d271d5b563186812778bb4",
+    "url": "/vue/img/icons/icon-512x512.png"
+  },
+  {
+    "revision": "d44a9c8e5e2e20a0ca6f446246caa85e",
+    "url": "/vue/img/icons/mstile-144x144.png"
+  },
+  {
+    "revision": "edd47346d9bea32c556085d66a05060f",
+    "url": "/vue/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "c5ce7854d263175f4dba5a6e3682740d",
+    "url": "/vue/img/icons/mstile-310x150.png"
+  },
+  {
+    "revision": "931effee26b055b608d126b3adceff54",
+    "url": "/vue/img/icons/mstile-310x310.png"
+  },
+  {
+    "revision": "6b028e865436b278cb46f0f9fd78ef56",
+    "url": "/vue/img/icons/mstile-70x70.png"
+  },
+  {
+    "revision": "0860745c989304acee0edc37ece8be5c",
+    "url": "/vue/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "78bcc898073f47148df112ae1967c9ff",
+    "url": "/vue/index.html"
+  },
+  {
+    "revision": "9d179a24b7a0cb0e0f28",
+    "url": "/vue/js/app.ab24dfba.js"
+  },
+  {
+    "revision": "10e3a082f389e3d06d56",
+    "url": "/vue/js/chunk-vendors.84b58a9d.js"
+  },
+  {
+    "revision": "dac91fa67ae65d74ca7ac815d90bfdd3",
+    "url": "/vue/licenses/fduhole-norms.pdf"
+  },
+  {
+    "revision": "3c600139ed4b98e7eb560d7c3741c932",
+    "url": "/vue/licenses/open-source-licenses.md"
+  },
+  {
+    "revision": "15f31b4a548b30b4666bca5444632a8e",
+    "url": "/vue/licenses/open-source-licenses.pdf"
+  },
+  {
+    "revision": "8d7c4bb7e3497fef26145dee059bc341",
+    "url": "/vue/licenses/privacy-policy.md"
+  },
+  {
+    "revision": "122a8d31b6ccaefc20f32917a8955057",
+    "url": "/vue/licenses/privacy-policy.pdf"
+  },
+  {
+    "revision": "e7af674e00d8e28a859e61d46a83f6ab",
+    "url": "/vue/licenses/terms-of-use.md"
+  },
+  {
+    "revision": "9a0dea4c92be3655c77871a7331230d3",
+    "url": "/vue/licenses/terms-of-use.pdf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,88 @@
+importScripts("/vue/precache-manifest.67b1d11f38bcefa9c7002d135c4d7db7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// self.addEventListener("message", (e)=>{
+//   if (e.data.action=='skipWaiting') {
+//     console.log('skip waiting')
+//     self.skipWaiting()
+//   }
+// })
+// noinspection JSUnresolvedVariable,JSUnresolvedFunction
+
+self.addEventListener('install', async () => {
+  await self.skipWaiting()
+  console.log('skip waiting')
+})
+
+self.addEventListener('active', async event => {
+  console.log('active')
+  console.log('active', event)
+  await self.clientsClaim()
+})
+
+let online = true
+setInterval(async () => {
+  try {
+    await fetch('online')
+    if (!online) {
+      online = true
+      const clients = await this.clients.matchAll()
+      console.log(clients)
+      if (clients && clients.length) {
+        clients.forEach((client) => {
+          client.postMessage({
+            msg: '网络连接已恢复, 请刷新以获取最新内容',
+            status: 'success'
+          })
+        })
+      }
+    }
+  } catch (e) {
+    if (online) {
+      online = false
+      const clients = await this.clients.matchAll()
+      console.log(clients)
+      if (clients && clients.length) {
+        clients.forEach((client) => {
+          client.postMessage({
+            msg: '网络已断开, 正在以离线模式浏览',
+            status: 'warning'
+          })
+        })
+      }
+    }
+  }
+}, 10000)
+
+// eslint-disable-next-line no-undef
+if (workbox) {
+  console.log('Workbox is loaded')
+
+  self.__precacheManifest = [].concat(self.__precacheManifest || [])
+  // eslint-disable-next-line no-undef
+  workbox.precaching.precacheAndRoute(self.__precacheManifest)
+
+  // eslint-disable-next-line no-undef
+  workbox.routing.registerRoute(
+    /^https:\/\/www\.fduhole\.com\/api\/.*/, // 匹配的路由
+    // eslint-disable-next-line no-undef
+    workbox.strategies.networkFirst({ cacheName: 'api' })
+  )
+
+  // eslint-disable-next-line no-undef
+  workbox.routing.registerRoute(
+    /^https:\/\/cdn\.jsdelivr\.net\/gh\/fduhole\/web@img\/.*/, // 匹配的路由
+    // workbox.strategies.staleWhileRevalidate({cacheName: 'img'}),
+    // eslint-disable-next-line no-undef
+    new workbox.strategies.CacheFirst({
+      plugins: [
+        // eslint-disable-next-line no-undef
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        })
+      ]
+    })
+  )
+} else {
+  console.log('Workbox didn\'t load')
+}
+
